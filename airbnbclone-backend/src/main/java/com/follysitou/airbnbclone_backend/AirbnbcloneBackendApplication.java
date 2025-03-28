ea4c01f46e1022d1d5085194f37c2a73247a1429
Carlo <carlogbossou93@gmail.com>
2025-03-28 10:37:26 +0000

    Ajout du backend

---
+++ airbnbclone-backend/src/main/java/com/follysitou/airbnbclone_backend/AirbnbcloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.follysitou.airbnbclone_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbcloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbnbcloneBackendApplication.class, args);
+	}
+
+}
